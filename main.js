fb42c06ceef3686bea5613080c862bb9f5e2c5fc
sudhaSN <95419061+sudhaSN@users.noreply.github.com>
2023-08-25 18:11:46 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,24 @@
+var latitude = 22.7868543,
+longitude = 88.3643296
+mapboxgl.accessToken = 'pk.eyJ1IjoiYXBvb3J2ZWxvdXMiLCJhIjoiY2ttZnlyMDgzMzlwNTJ4a240cmEzcG0xNyJ9.-nSyL0Gy2nifDibXJg4fTA';
+// Initializing mapbox
+var map = new mapboxgl.Map({
+    container: 'map',
+    style: 'mapbox://styles/mapbox/streets-v11',
+    center: [longitude, latitude],
+    zoom: 16
+})
+map.addControl(
+    new mapboxgl.GeolocateControl({
+        positionOptions: {
+            enableHighAccuracy: true
+        },
+        trackUserLocation: true
+    })
+)
+map.addControl(
+    new MapboxDirections({
+        accessToken: mapboxgl.accessToken
+    }),
+    'top-left'
+)
